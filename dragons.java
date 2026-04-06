ef1267df491961fd300702b3f0483ba348e783a1
Stracimir <128100069+StracimirNikolaevich@users.noreply.github.com>
2026-04-06 22:13:07 +0200

    Refactor Dragon class and update attributes

--- dragons.java
+++ dragons.java
@@ -1,32 +1,39 @@
- 
-package dragons;
+package dragon;
 
-public class dragons {
-    private int id;
+public class Dragon {
+	private int id;
     private String name;
     private String type;
     private int powerLevel;
-
-    public dragons(int id, String name, String type, int powerLevel) {
+    private int atk;
+    private int def;
+    
+    public static final String[] VALID_TYPES = {"fire", "water", "earth", "wind", "light", "darkness"};
+    
+    public Dragon(int id, String name, String type, int level, int atk, int def) {
         this.id = id;
         this.name = name;
         this.type = type;
-        this.powerLevel = powerLevel;
+        this.powerLevel = level;
+        this.atk = atk;
+        this.def = def;
     }
-
-    // Getters and Setters
     public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
     public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
     public String getType() { return type; }
-    public void setType(String type) { this.type = type; }
-    public int getPowerLevel() { return powerLevel; }
-    public void setPowerLevel(int powerLevel) { this.powerLevel = powerLevel; }
+    public int getLevel() { return powerLevel; }
+    public int getAtk() { return atk; }
+    public int getDef() { return def; }
 
+    public void setName(String name) { this.name = name; }
+    public void setType(String type) { this.type = type; }
+    public void setLevel(int level) { this.powerLevel = powerLevel; }
+    public void setAtk(int atk) { this.atk = atk; }
+    public void setDef(int def) { this.def = def; }
+    
     @Override
     public String toString() {
-        return String.format("ID: %d | Name: %-10s | Type: %-8s | Power: %d", 
-                id, name, type, powerLevel);
+        return String.format("ID:%d | Name:%s | Type:%s | Level:%d | ATK:%d | DEF:%d",
+                id, name, type, powerLevel, atk, def);
     }
 }
